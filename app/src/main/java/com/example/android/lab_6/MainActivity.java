4981b9ec842bc201612a350a1382785b077facec
Vera Wei <vera.wei@gmail.com>
2020-03-09 15:02:48 -0500

    Milestones 1&2

--- app/src/main/java/com/example/android/lab_6/MainActivity.java
+++ app/src/main/java/com/example/android/lab_6/MainActivity.java
@@ -1,13 +1,91 @@
 package com.example.android.lab_6;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MainActivity extends FragmentActivity {
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+
+    //private final LatLng mDestinationLatLng = new LatLng(-33.8523341, 151.2106085);
+    private final LatLng mDestinationLatLng = new LatLng(43.0753564,-89.4041781);
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+                // code to display marker
+                googleMap.addMarker(new MarkerOptions()
+                        .position(mDestinationLatLng)
+                        .title("Destination"));
+                displayMyLocation(googleMap);
+        });
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
     }
+
+    private void displayMyLocation(GoogleMap mMap) {
+        int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION);
+        if(permission == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+        else{
+            mFusedLocationProviderClient.getLastLocation().addOnCompleteListener(this, task -> {
+                Location mLastKnownLocation = task.getResult();
+                if (task.isSuccessful() && mLastKnownLocation != null) {
+                    mMap.addPolyline(new PolylineOptions().add(
+                            new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()), mDestinationLatLng));
+                    LatLng currentLatLng = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(currentLatLng).title("Current"));
+                }
+            });
+//            mFusedLocationProviderClient.getLastLocation()
+//                    .addOnCompleteListener(this, task -> {
+//                        Location mLastKnownLocation = task.getResult();
+//                        if (task.isSuccessful() && mLastKnownLocation != null){
+//                            mMap.addPolyline(new PolylineOptions().add(
+//                                    new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()), mDestinationLatLng));
+//                        }
+//                        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+//                        LatLng CurrentP = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
+//                        mapFragment.getMapAsync(googleMap -> {
+//                            mMap = googleMap;
+//                            //marker code
+//                            googleMap.addMarker(new MarkerOptions()
+//                                    .position(CurrentP)
+//                                    .title("Current"));
+//                            displayMyLocation();
+//                        });
+//                    });
+        }
+    }
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION){
+//            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+//                displayMyLocation(googleMap);
+//            }
+//        }
+//    }
 }
